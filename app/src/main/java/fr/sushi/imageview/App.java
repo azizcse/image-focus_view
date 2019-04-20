ccb7b5c95ae7b7ab9aa064f82886a5c30cad3de0
SONY <imazizul@gmail.com>
2019-04-21 02:23:48 +0600

    first commit

---
+++ app/src/main/java/fr/sushi/imageview/App.java
@@ -0,0 +1,10 @@
+package fr.sushi.imageview;
+
+import android.support.multidex.MultiDexApplication;
+
+public class App extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
